dd3604e69ed83f7c4a618021b7270aa4fff908d0
mhdix <domanloom@gmail.com>
2024-04-09 01:39:39 -0700

    fixed many style

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// const toggler = document.querySelector(".nav__toggler");
-// const navbar = document.querySelector(".nav")
-// toggler.addEventListener("click" , (e) => {
-//     console.log("click ");
-//     navbar.classList.toggle("nav__expanded")
-// })
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav")
+toggler.addEventListener("click" , (e) => {
+    console.log("click ");
+    navbar.classList.toggle("nav__expanded")
+})
 
 {/* <ul>
     <li class="tab active" data-target-value="tab-one">tab -1</li>

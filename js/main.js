1799764f605de9c4e99de88a59d7da51af0497a1
mhdix <domanloom@gmail.com>
2024-04-08 21:01:52 -0700

    complate tab content section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".nav")
-toggler.addEventListener("click" , (e) => {
-    console.log("click ");
-    navbar.classList.toggle("nav__expanded")
+// const toggler = document.querySelector(".nav__toggler");
+// const navbar = document.querySelector(".nav")
+// toggler.addEventListener("click" , (e) => {
+//     console.log("click ");
+//     navbar.classList.toggle("nav__expanded")
+// })
+
+{/* <ul>
+    <li class="tab active" data-target-value="tab-one">tab -1</li>
+    <li class="tab" data-target-value="tab-two">tab -2</li>
+    <li class="tab" data-target-value="tab-three">tab -3</li>
+</ul>
+
+<div class="">
+    <div class="tab-content active" id="tab-one">tab-one</div>
+    <div class="tab-content" id="tab-two">tab-two</div>
+    <div class="tab-content" id="tab-three">tab-three</div>
+</div> */}
+
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
+tabs.forEach((tab) => {
+    tab.addEventListener('click' , (e) => {
+        const targeTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+        
+        tabs.forEach((tab) => tab.classList.remove('active'));
+        tabContents.forEach((tabContent) => tabContent.classList.remove('active') );
+
+        tab.classList.add('active');
+        targeTabContent.classList.add('active');
+    }) 
 })
